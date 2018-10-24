e11ae49570727b521d7532a3d05b1addd8592bdd
Julian Bautista <julian.bautista@port.ac.uk>
2018-10-24 12:06:31 +0100

    adding export function

---
+++ bin/export_multipoles.py
@@ -0,0 +1,20 @@
+from cf_tools import Multipoles
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-i', '--input', help='Input correlation function file (CUTE output)')
+parser.add_argument('-o', '--output', help='Output multipoles file')
+parser.add_argument('--rebin_r',
+                    help='Make rebin_r bins into a single bin',
+                    type=int, default=5)
+parser.add_argument('--shift_r', help='Starts rebinning from bin number shift_r', \
+                    type=int, default=0)
+args = parser.parse_args()
+
+m = Multipoles(args.input, rebin_r=args.rebin_r, shift_r=args.shift_r, multipoles=0)
+m.export(args.output)
+
+
+
+
